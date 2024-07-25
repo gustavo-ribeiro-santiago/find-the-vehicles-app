11964314b22da521bcb68c13c7ad97493f9848e0
gustavo-ribeiro-santiago <gustavorisa@poli.ufrj.br>
2024-07-25 08:22:55 -0300

    adjusted next/script

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -27,11 +28,11 @@ export default function RootLayout({ children }) {
       </Head>
       <body className={inter.className}>
         {children}
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
           crossOrigin="anonymous"
-        ></script>
+        ></Script>
       </body>
     </html>
   );
